c22bd83c2dac94244a19055e3b9b057b10363773
lululufr <lucas.lm.miller@proton.me>
2025-08-03 15:33:23 +0200

    Full-update NixOS configuration $(date +%Y-%m-%d)

--- modules/system/shell.nix
+++ modules/system/shell.nix
@@ -10,7 +10,7 @@
     shellAliases = {
       update = "sudo nixos-rebuild switch --upgrade --flake /etc/nixos#tardis-nix";
       update-git = "cd /etc/nixos && sudo git add -A && sudo git commit -m 'Update NixOS configuration $(date +%Y-%m-%d)' && sudo git push";
-      update-full = "cd /etc/nixos && sudo git add -A && sudo git commit -m 'Full-update NixOS configuration $(date +%Y-%m-%d)' && sudo git push sudo nixos-rebuild switch --upgrade --flake /etc/nixos#tardis-nix && cd";
+      update-full = "cd /etc/nixos && sudo git add -A && sudo git commit -m 'Full-update NixOS configuration $(date +%Y-%m-%d)' && sudo git push && sudo nixos-rebuild switch --upgrade --flake /etc/nixos#tardis-nix && cd";
       list-gen = "nixos-rebuild list-generations";
       del-gen = "sudo nix-collect-garbage -d && sudo nixos-rebuild switch --flake /etc/nixos#tardis-nix";
       
