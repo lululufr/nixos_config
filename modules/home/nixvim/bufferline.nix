f6d567c35419e150378ad20ce2485d41ef41898f
lululufr <lucas.lm.miller@proton.me>
2025-08-03 15:32:15 +0200

    Full-update NixOS configuration $(date +%Y-%m-%d)

--- modules/home/nixvim/bufferline.nix
+++ modules/home/nixvim/bufferline.nix
@@ -3,7 +3,7 @@
     enable = true;
 
 
-           offsets = [
+   settings.options.offsets = [
           { filetype = "neo-tree";         text = "Neo-tree"; highlight = "Directory"; text_align = "left"; }
           { filetype = "snacks_layout_box"; }
         ];
