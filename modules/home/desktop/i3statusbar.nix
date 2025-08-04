bbd6ec1402d0a5e479b0d46e0a11168dc3ac8dad
lululufr <lucas.lm.miller@proton.me>
2025-08-04 22:07:09 +0200

    Full-update NixOS configuration $(date +%Y-%m-%d)

--- modules/home/desktop/i3statusbar.nix
+++ modules/home/desktop/i3statusbar.nix
@@ -6,18 +6,15 @@
       blocks = [
         {
           block = "focused_window";
-          max_width = 50;
         }
         {
           block = "cpu";
           interval = 1;
-          format = " $icon $utilization ";
-          format_alt = " $icon $frequency ";
+          format = "🖥️ $utilization";
         }
         {
           block = "memory";
-          format = " $icon $mem_used_percents ";
-          format_alt = " $icon $mem_used/$mem_total ";
+          format = "🧠 $mem_used_percents";
           interval = 5;
           warning_mem = 80.0;
           critical_mem = 95.0;
@@ -26,34 +23,29 @@
           block = "disk_space";
           path = "/";
           info_type = "available";
-          format = " $icon $available ";
-          format_alt = " $icon $used/$total ";
+          format = "💾 $available";
           warning = 20.0;
           alert = 10.0;
         }
         {
           block = "net";
-          format = " $icon {$signal_strength $ssid |Wired} ";
-          format_alt = " $icon $speed_down/$speed_up ";
+          format = "🌐 {$signal_strength $ssid |Wired}";
           interval = 5;
         }
         {
           block = "sound";
-          format = " $icon $volume ";
+          format = "🔊 $volume";
           headphones_indicator = true;
         }
         {
           block = "battery";
           interval = 10;
-          format = " $icon $percentage ";
-          full_format = " $icon ";
-          empty_format = " $icon $percentage ";
-          not_charging_format = " $icon $percentage ";
+          format = "🔋 $percentage";
         }
         {
           block = "time";
           interval = 60;
-          format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+          format = "$timestamp.datetime(f:'%a %d/%m %R')";
         }
       ];
       settings = {
