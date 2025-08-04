e834f7ac608449c5fa4357ea207a00b2290e3263
lululufr <lucas.lm.miller@proton.me>
2025-08-04 22:27:10 +0200

    Full-update NixOS configuration $(date +%Y-%m-%d)

--- modules/home/nixvim/plugins/lualine.nix
+++ modules/home/nixvim/plugins/lualine.nix
@@ -3,17 +3,19 @@
     enable = true;
     settings = {
       options = {
-        theme        = "auto";
-        globalstatus = { __raw = "vim.o.laststatus == 3" ; };
+        theme = "auto";
+        globalstatus = { __raw = "vim.o.laststatus == 3"; };
       };
-
       sections = {
         lualine_a = [ "mode" ];
         lualine_b = [ "branch" ];
-      
+        lualine_c = [ "filename" ];
+        lualine_x = [ "encoding" "fileformat" "filetype" ];
+        lualine_y = [ "progress" ];
+        lualine_z = [ "location" ];
+      }; 
       extensions = [ "neo-tree" "lazy" "fzf" ];
     };
   };
-};
 }
 
